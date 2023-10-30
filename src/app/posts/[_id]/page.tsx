82924536dad8bbd1d05f1de7f98edbcda9348c41
Fernando <21fernandinhow@gmail.com>
2023-10-30 11:55:42 -0300

    fix date format

--- src/app/posts/[_id]/page.tsx
+++ src/app/posts/[_id]/page.tsx
@@ -17,11 +17,13 @@ async function getPostById(_id:string) {
 
 export default async function PostPage({ params }: { params: { _id: string } }) {
     const post = await getPostById(params._id);
+
+    const formattedDate = new Date(post.date).toLocaleDateString();
     
     return(
         <section id="post">
             <h2>{post.title}</h2>
-            <h4>{new Date(post.date).toLocaleDateString()}</h4>
+            <h4>{formattedDate}</h4>
             {post.content.split("\n").map((item:string, index:number) => (
                 <p key={index}>{item}</p>
             ))}
