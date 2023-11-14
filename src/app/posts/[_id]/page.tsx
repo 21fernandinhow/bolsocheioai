32a9fa6da92d72dadbb8a69ee981fa42249b2812
21fernandinhow <21fernandinhow@gmail.com>
2023-11-14 19:27:39 -0300

    optimize seo

--- src/app/posts/[_id]/page.tsx
+++ src/app/posts/[_id]/page.tsx
@@ -1,3 +1,15 @@
+import type { Metadata } from 'next'
+
+export async function generateMetadata({ params }: { params: { _id: string }}):Promise<Metadata>{
+
+    const post = await getPostById(params._id);
+
+    return{
+        title: post.title,
+        description: post.content.slice(0,150)
+    }
+}
+
 async function getPostById(_id:string) {
 
     const response = await fetch(`${process.env.NEXT_PUBLIC_POSTS_URL}/${_id}`, {
@@ -22,11 +34,14 @@ export default async function PostPage({ params }: { params: { _id: string } }) 
     
     return(
         <section id="post">
+
             <h2>{post.title}</h2>
             <h4>{formattedDate}</h4>
+
             {post.content.split("\n").map((item:string, index:number) => (
                 <p key={index}>{item}</p>
             ))}
+
         </section>
     )
 }
