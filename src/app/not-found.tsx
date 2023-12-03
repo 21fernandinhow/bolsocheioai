6b5339675989800ecc86ecc2dc2dcd309046a89b
21fernandinhow <21fernandinhow@gmail.com>
2023-12-03 10:06:52 -0300

    Add custom metadata to page 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link"
 
+export const metadata = {
+    title: "Página não encontrada | Bolso Cheio A.I",
+    description: `O Bolso Cheio A.I. é uma inovação no mundo das finanças e investimentos. Este blog é único, pois todo o seu conteúdo é gerado 
+    automaticamente por uma inteligência artificial de ponta, baseada em GPT, e capaz de produzir artigos perspicazes sobre educação financeira e 
+    princípios de investimentos de maneira ágil e precisa.`,
+}
+
 export default function NotFoundPage() {
 	return (
         <section id="not-found">
