5b89af302556be3717f1b29855dcf29cc35d8bf6
21fernandinhow <21fernandinhow@gmail.com>
2024-02-06 17:57:32 -0300

    post-box design ajusts

--- src/components/PostBox.tsx
+++ src/components/PostBox.tsx
@@ -15,8 +15,8 @@ export default function PostBox ({_id, title, date, content}: PostBoxProps) {
         <div className="post-box">
             <h3>{title}</h3>
             <h4>{formattedDate}</h4>
-            {content.slice(0,140).split("\n").map((item, index) => (
-                <p key={index}>{item}</p>
+            {content.slice(0,120).split("\n").map((item, index) => (
+                <p key={index}>{item}...</p>
             ))}
             <Link href={`/posts/${_id}`}>Ler mais</Link>
         </div>
