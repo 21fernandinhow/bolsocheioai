a1e16fc4257bf174d9534ea944f3c724b4cb8886
21fernandinhow <21fernandinhow@gmail.com>
2024-05-13 21:17:10 -0300

    improve provisory page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link"
 import Image from "next/image"
+import ThemeButton from "@/components/ThemeButton"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 // async function getPosts() {
 
@@ -25,12 +28,18 @@ export default async function ProvisoryHome() {
       <section>
         <Link href="https://bolsocheio.ai">
           <Image src={'/logo.png'}
-              width={90}
-              height={60}
+              width={120}
+              height={80}
               alt={"Bol$o Cheio AI"}          
           />
         </Link>
         <h1>Em breve...</h1>
+        <div className="options">
+          <Link href="https://www.instagram.com/bolsocheio.ai" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} />
+          </Link>
+          <ThemeButton/>
+        </div>
       </section>
   )
 }
