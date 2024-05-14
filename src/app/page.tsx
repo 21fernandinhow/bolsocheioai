9875bf2039d2daee25289357ac17a889a4a03bc0
21fernandinhow <21fernandinhow@gmail.com>
2024-05-13 21:09:19 -0300

    provisory page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,37 @@
-import PostList from "@/components/PostList";
+import Link from "next/link"
+import Image from "next/image"
 
-async function getPosts() {
+// async function getPosts() {
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_POSTS_URL}`, {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
-      },
-      next: { revalidate: 300 }
-  });
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_POSTS_URL}`, {
+//       method: 'GET',
+//       headers: {
+//           'Content-Type': 'application/json',
+//           'x-api-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
+//       },
+//       next: { revalidate: 300 }
+//   });
 
-  if (!response.ok) {
-      throw new Error('Erro ao buscar posts')
-  }
+//   if (!response.ok) {
+//       throw new Error('Erro ao buscar posts')
+//   }
 
-  return response.json();
-};
+//   return response.json();
+// };
 
-export default async function Home() {
-
-  const posts = await getPosts()
+export default async function ProvisoryHome() {
 
   return (
-    <section id="home">
-      <div className="header">
-        <h2>Bem-vindo ao Bolso Cheio A.I.</h2>
-        <p>Seu blog de finanças com conteúdo gratuito 100% produzido por inteligência artificial.</p>
-      </div>
-      <PostList posts={posts.reverse()}/>
-    </section>
+      <section>
+        <Link href="https://bolsocheio.ai">
+          <Image src={'/logo.png'}
+              width={90}
+              height={60}
+              alt={"Bol$o Cheio AI"}          
+          />
+        </Link>
+        <h1>Em breve...</h1>
+      </section>
   )
 }
 
