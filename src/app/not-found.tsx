9c838850799375fcfa101795b745144e5fbcd0e3
21fernandinhow <21fernandinhow@gmail.com>
2024-05-26 12:36:13 -0300

    update layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFoundPage() {
 	return (
         <section id="not-found">
             <h1>Página não encontrada</h1>
-            <p>Parece que o conteúdo que você está buscando não existe (ainda) ... </p>
+            <p>Parece que o conteúdo que você está buscando não existe ... </p>
             <Link href='/' className="btn-lg">Voltar ao Início</Link>
         </section>
     )
