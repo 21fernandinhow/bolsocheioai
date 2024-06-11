c3a209d5a24223b808537ff3b0f9e4daa498be1d
21fernandinhow <21fernandinhow@gmail.com>
2024-06-11 20:06:05 -0300

    changed share message

--- src/components/SharePostButtons.tsx
+++ src/components/SharePostButtons.tsx
@@ -13,7 +13,7 @@ export type PostToShare = {
 export default function SharePostButtons ({id, title}: PostToShare){
 
     const postUrl = `https://bolsocheio.ai/posts/${id}`
-    const shareMessage = `Esse post do Bolso Cheio A.I vai te interessar: *${title}*. Acesse agora: ${postUrl}`
+    const shareMessage = `${title}: ${postUrl}`
 
     return (
         <div className='share-post'>
