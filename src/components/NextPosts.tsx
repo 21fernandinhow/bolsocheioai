a1ac16287b02823564eef365002efa0310e5d66f
21fernandinhow <21fernandinhow@gmail.com>
2024-09-08 12:16:06 -0300

    fixed crashing home

--- src/components/NextPosts.tsx
+++ src/components/NextPosts.tsx
@@ -1,20 +1,20 @@
 async function getNextPosts(){
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_POSTS_URL}/next`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
+        const response = await fetch(`${process.env.NEXT_PUBLIC_POSTS_URL}/next`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
+            }
+        });
+    
+        if (!response.ok) {
+            throw new Error('Erro ao buscar leads')
         }
-      });
   
-      if (!response.ok) {
-          throw new Error('Erro ao buscar leads')
-      }
-  
-      return response.json();
+        return response.json();
     } catch (error) {
-      console.error(error)
+        console.error(error)
     }
 }
 
@@ -25,11 +25,11 @@ export default async function NextPosts () {
     return(
         <section className="item-box next-posts">
             <h2>Próximos Posts:</h2>
-            {nextPosts.map((item: {title: string, keywords: []}, index: number) => (
+            {nextPosts && nextPosts.map((item: {title: string, keywords: []}, index: number) => (
                 <div key={index} className="next-post-item">
-                    <h3 key={index}>{item.title}</h3>
+                    <h3 key={index}>{item?.title}</h3>
                     <div className="keywords">
-                        {item.keywords.map((keyword, index) => <span key={index}>{keyword}</span>)}
+                        {item?.keywords.map((keyword, index) => <span key={index}>{keyword}</span>)}
                     </div>
                 </div>
             ))}
