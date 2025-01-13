970d83e4b08ef59bb73251e0afb996e9031db616
Fernando Carvalho <21fernandinhow@gmail.com>
2025-01-12 21:10:52 -0300

    dynamic sitemap

---
+++ src/app/api/sitemap.xml/route.ts
@@ -0,0 +1,72 @@
+import { PostBoxProps } from '@/components/PostBox';
+import { NextResponse } from 'next/server';
+
+
+export async function getPosts() {
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_POSTS_URL}`, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
+      },
+      next: { revalidate: 300 }
+  });
+
+  if (!response.ok) {
+      throw new Error('Erro ao buscar posts')
+  }
+
+  return response.json();
+};
+
+export async function GET() {
+  const baseUrl = 'https://bolsocheio.ai';
+  
+  // Obtenha os posts do seu banco de dados ou fonte de dados.
+  const posts = await getPosts()
+
+  // Gera o conteúdo do sitemap em XML.
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${baseUrl}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/about</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.9</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/contact</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.9</priority>
+      </url>
+      ${posts
+        .map(
+          (post: PostBoxProps) => `
+        <url>
+          <loc>${baseUrl}/${post._id}</loc>
+          <lastmod>${post.date}</lastmod>
+          <changefreq>weekly</changefreq>
+          <priority>0.8</priority>
+        </url>
+      `
+        )
+        .join('')}
+    </urlset>
+  `.trim();
+
+  // Retorna o sitemap como resposta.
+  return new NextResponse(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
